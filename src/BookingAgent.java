579038c2421c82b596635e32f147f1258a4a53a8
mahabirguptanew <mahabirg81@gmail.com>
2026-06-25 13:18:58 +0800

    Implemented method to book single and multiple seats in BookingAgent.java file

--- src/BookingAgent.java
+++ src/BookingAgent.java
@@ -1,3 +1,5 @@
+import java.util.Set;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class BookingAgent {
@@ -6,18 +8,44 @@ public class BookingAgent {
         int totalSeats=100;
         Theatre citySquareMall = new Theatre("City Square Mall",rows,totalSeats);
         citySquareMall.printSeatMap();
-        citySquareMall.bookSeat(citySquareMall,'A',3);
-        citySquareMall.bookSeat(citySquareMall,'A',3);
-        citySquareMall.bookSeat(citySquareMall,'B',1);
-        citySquareMall.bookSeat(citySquareMall,'B',11);
-        citySquareMall.bookSeat(citySquareMall,'M',1);
+        bookSeat(citySquareMall,'A',3);
+        bookSeat(citySquareMall,'A',3);
+        bookSeat(citySquareMall,'B',1);
+        bookSeat(citySquareMall,'B',11);
+        bookSeat(citySquareMall,'M',1);
+        System.out.println();
+        bookSeats(citySquareMall,4,'B',3,10);
+        bookSeats(citySquareMall,6,'B','C',3,10);
+        bookSeats(citySquareMall,4,'B',1,10);
+        bookSeats(citySquareMall,4,'B','C',1,10);
+        bookSeats(citySquareMall,1,'B','C',1,10);
+        bookSeats(citySquareMall,4,'M','Z',1,10);
+        bookSeats(citySquareMall,10,'A','E',1,10);
 
 
-//        citySquareMall.reserveSeat('A',8);
-//        System.out.println();
-//        citySquareMall.reserveSeat('B',1);
-//        System.out.println();
 
+    }
 
+    private static void bookSeat(Theatre theatre, char row, int seatNo){
+        String seat = theatre.reserveSeat(row,seatNo);
+        if (seat!=null){
+            System.out.println("Congratulations! Your reserve seat is "+seat);
+            theatre.printSeatMap();
+        }else {
+            System.out.println("Sorry! Unable to reserve "+row+seatNo);
+        }
+    }
+
+    private static void bookSeats(Theatre theatre,int tickets,char minRow,int minSeat,int maxSeat){
+        bookSeats(theatre,tickets,minRow,minRow,minSeat,maxSeat);
+    }
+    private static void bookSeats(Theatre theatre,int tickets,char minRow,char maxRow,int minSeat,int maxSeat){
+        Set<Seat> seats=theatre.reserveSeats(tickets,minRow,maxRow,minSeat,maxSeat);
+        if (seats!=null){
+            System.out.println("Congratulations! Your reserved seats are " + seats);
+            theatre.printSeatMap();
+        }else {
+            System.out.println("Sorry! No matching contiguous seats in rows: "+minRow+" - "+maxRow);
+        }
     }
 }
--- src/Theatre.java
+++ src/Theatre.java
@@ -1,4 +1,5 @@
 import java.util.NavigableSet;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Theatre {
@@ -49,13 +50,47 @@ public class Theatre {
         }
         return null;
     }
-    public static void bookSeat(Theatre theatre, char row, int seatNo){
-        String seat = theatre.reserveSeat(row,seatNo);
-        if (seat!=null){
-            System.out.println("Congratulations! Your reserve seat is "+seat);
-            theatre.printSeatMap();
-        }else {
-            System.out.println("Sorry! Unable to reserve "+row+seatNo);
+
+    public boolean validate(int numOfSeats,char first,char last, int min,int max){
+        boolean result=( min>0 || seatsPerRow >= numOfSeats|| (max-min+1) >= numOfSeats);
+        result = result && seats.contains(new Seat(first,min));
+        if (!result){
+            System.out.printf("Invalid! %1$d seats between "+"%2$c[%3$d-%4$d]-%5$c[%3$d-%4$d] Try again",numOfSeats,first,min,max,last);
+            System.out.printf(": Seat must be between %s and %s%n",seats.first().getSeatNum(),seats.last().getSeatNum());
         }
+        return result;
+    }
+    public Set<Seat> reserveSeats(int numOfSeats, char minRow, char maxRow, int minSeat, int maxSeat){
+        char lastValid=seats.last().getSeatNum().charAt(0);// to get the last alphabet of the last seat
+        maxRow=(maxRow<lastValid)?maxRow:lastValid;
+        if (!validate(numOfSeats,minRow,maxRow,minSeat,maxSeat)){
+            return null;
+        }
+        NavigableSet<Seat> selected=null;
+        for (char letter=minRow; letter<=maxRow;letter++){
+            NavigableSet<Seat> contiguous = seats.subSet(new Seat(letter,minSeat),true,new Seat(letter,maxSeat),true);
+            int index=0;
+            Seat first=null;
+            for (Seat currentSeat:contiguous) {
+                if (currentSeat.isReserved()){
+                    index=0;
+                    continue;
+                }
+                first=(index==0)?currentSeat:first;
+                if (++index==numOfSeats){
+                    selected=contiguous.subSet(first,true,currentSeat,true);
+                    break;
+                }
+            }
+        if (selected!=null){
+            break;
+        }
+        }
+        Set<Seat> reservedSeats=null;
+        if (selected!=null){
+            selected.forEach(seat -> seat.setReserved(true));
+            reservedSeats=new TreeSet<>(selected);
+        }
+        return reservedSeats;
     }
 }
